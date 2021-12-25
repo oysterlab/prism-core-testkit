ff79385498d0c723737a32e22bcc56c686f67f3d
oysterlab <oysterlaboratory@gmail.com>
2021-12-26 00:56:38 +0900

    e

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Components from "./components"
 import Component from "./components/Component"
-import { selectLayerByName, selectLayersByName } from "./utils/Selector"
+import { selectLayersByName } from "./utils/Selector"
 
 const sketch2json = require('sketch2json')
 const fs = require('fs')
@@ -10,23 +10,20 @@ const sketchFilePath = "./sample/sample1.sketch"
 async function main() {
   const data = await sketch2json(fs.readFileSync(sketchFilePath))
 
+  const meta:any[] = []
+  
   Object.values(Components).forEach((ComponentClass:typeof Component) => {
     ComponentClass.alias.forEach((alias:string) => {
       const componentLayerNodes = selectLayersByName(data, '@' + alias, true)
       componentLayerNodes.forEach((componentLayerNode:any) => {
         const instance = new ComponentClass(componentLayerNode.value, componentLayerNode.path)
         const result = instance.toJson()
-        console.log(result)
-        console.log() 
+        meta.push(result)
       })
     })
   })
 
-  // const componentLayerNode = selectLayerByName(data, '@C_ImageBox_01', true)
-  // if (!componentLayerNode) return
-  // const instance = new CImageBox01(componentLayerNode.value, componentLayerNode.path)
-  // const result = instance.toJson()
-  // console.log(result)  
+  console.log(meta)
 }
 
 main()
